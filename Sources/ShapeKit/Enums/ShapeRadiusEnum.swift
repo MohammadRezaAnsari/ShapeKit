8036f5858b6b410149d9398922ab7fa843ea9a44
Mohammad Reza Ansary <mareza.ansary@gmail.com>
2021-02-22 14:26:55 +0330

    feat: Implement ShapeCornersEnum feat: Implement ShapeRadiusEnum

---
+++ Sources/ShapeKit/Enums/ShapeRadiusEnum.swift
@@ -0,0 +1,12 @@
+//
+//  ShapeRadiusEnum.swift
+//  
+//
+//  Created by MohammadReza Ansary on 2/22/21.
+//
+
+public enum ShapeRadiusEnum: Int {
+    case none = 0
+    case low = 8
+    case large = 16
+}
